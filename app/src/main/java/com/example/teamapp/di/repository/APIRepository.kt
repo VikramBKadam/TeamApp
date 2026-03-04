ad9fa451cae7234a399f30b6b9087d5bbab34066
Vikram <kadamvikram147@gmail.com>
2026-03-04 16:18:22 +0900

    added comment

--- app/src/main/java/com/example/teamapp/di/repository/APIRepository.kt
+++ app/src/main/java/com/example/teamapp/di/repository/APIRepository.kt
@@ -5,7 +5,7 @@ import com.example.teamapp.model.Role
 import javax.inject.Inject
 
 class APIRepository @Inject constructor(private val apiService: APIService) {
-    suspend fun getTeam(teamId: String) = apiService.getTeam(teamId)
+    suspend fun getTeam(teamId: String) = apiService.getTeam(teamId) //
     suspend fun getInvitation(teamId: String,role: Role) = apiService.getInvitationURL(teamId, role)
 
 
